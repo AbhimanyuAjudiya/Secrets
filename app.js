c72f2d21492b268c32f005c23d010c8b12998b9f
Abhimanyu Ajudiya <88022199+AbhimanyuAjudiya@users.noreply.github.com>
2022-06-24 15:44:29 +0530

    Using hashing technic to secure our user password

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
 
 const app = express();
 
@@ -22,9 +24,6 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-//this should be before making model
-const encryptedSecret = process.env.SECRET;
-userSchema.plugin(encrypt,{secret:encryptedSecret, encryptedFields: ["password"] });// you can encrypet multiple fields by adding it in "[]".
 
 const User = mongoose.model("User", userSchema);
 
@@ -39,17 +38,20 @@ app.get("/register", function (req, res){
   });
  
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
 
-  newUser.save(function(err){
-    if (err){
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+  
+    newUser.save(function(err){
+      if (err){
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 });
 
@@ -63,13 +65,15 @@ app.post("/login", function(req, res){
       console.log(err);
     } else {
       if(foundUser){
-        if (foundUser.password === password){
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result){
+          if (result === true){
+            res.render("secrets");
+          }
+        });
       }
     }
-  })
-})
+  });
+});
 
 
 
